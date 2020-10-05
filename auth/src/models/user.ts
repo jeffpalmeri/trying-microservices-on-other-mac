adaa2aac5b1367353e895ef3266c9d842e96bdc2
Jeff Palmeri <jeffpalmeri184@gmail.com>
2020-10-04 21:36:01 -0400

    add password hashing middleware to user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../utilities/password';
 
 // interface that describes the properties that are required to create a new User.
 interface UserAttrs {
@@ -26,6 +27,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// mongoose middleware function
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
